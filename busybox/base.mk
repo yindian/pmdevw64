f5463e2633eb03a5f638a071e938f649e0b671d8
Yin Dian <yindian@gmail.com>
2020-01-10 21:38:12 +0800

    update busybox from upstream

--- busybox/base.mk
+++ busybox/base.mk
@@ -1,10 +1,12 @@
 EXE = busybox.exe
 SRCDIR = busybox-w32
 SRCURL = https://frippery.org/files/busybox/$(SRCFILE)
-SRCFILE = busybox-w32-FRP-3128-g241d4d4ac.tgz
-SRCMD5 = f9d107e52b6174ba7c9024c2d3c941f6
+SRCFILE = busybox-w32-FRP-3329-gcf0fa4d13.tgz
+SRCMD5 = 25258380f8cc3d54aa1531da00a4274d
 PATCHES = patch_alt_cp_ts.diff
 CONFIG = config
+SED = sed
+config_to_sed = $(shell $(SED) 's|^\(CONFIG[^ =]*\) *=\(.*\)|/\\<\1\\>/s/.*/\1=\2/;|; s|^\# *\(CONFIG[^ =]*\)\(.*\)|/\\<\1\\>/s/.*/\# \1\2/;|' $(1))
 
 .PHONY: clean
 
@@ -27,8 +29,8 @@ source/$(SRCDIR)/Makefile: $(SOURCEDIR)/$(SRCFILE)
 	touch -r $@ $<
 
 source/$(SRCDIR)/.config: $(CONFIG) source/$(SRCDIR)/Makefile
-	cp -p $< $@
-	$(MAKE) -C source/$(SRCDIR) oldconfig
+	$(MAKE) -C source/$(SRCDIR) mingw64_defconfig
+	$(SED) -i '$(call config_to_sed, $<)' $@
 
 $(SOURCEDIR)/$(SRCFILE):
 	@mkdir -p $(@D)
