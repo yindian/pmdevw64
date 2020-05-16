b43ae0f9ecb7a9320d0c3df3865126fa6d16ced0
Yin Dian <yindian@gmail.com>
2020-05-17 00:22:55 +0800

    fix build under os x

--- busybox/base.mk
+++ busybox/base.mk
@@ -5,7 +5,11 @@ SRCFILE = busybox-w32-FRP-3329-gcf0fa4d13.tgz
 SRCMD5 = 25258380f8cc3d54aa1531da00a4274d
 PATCHES = patch_alt_cp_ts.diff
 CONFIG = config
+ifneq ($(shell which gsed 2>/dev/null),)
+SED = gsed
+else
 SED = sed
+endif
 config_to_sed = $(shell $(SED) 's|^\(CONFIG[^ =]*\) *=\(.*\)|/\\<\1\\>/s/.*/\1=\2/;|; s|^\# *\(CONFIG[^ =]*\)\(.*\)|/\\<\1\\>/s/.*/\# \1\2/;|' $(1))
 
 .PHONY: clean
@@ -27,6 +31,10 @@ source/$(SRCDIR)/Makefile: $(SOURCEDIR)/$(SRCFILE)
 	tar -C source -xzf $<
 	for c in $(PATCHES); do cat $$c | (cd $(@D); patch -p1); done
 	touch -r $@ $<
+ifeq ($(SED),gsed)
+	$(SED) -i 's/\bsed\b/gsed/g' $@
+	find source/$(SRCDIR)/scripts -type f | xargs $(SED) -i 's/\bsed\b/gsed/g'
+endif
 
 source/$(SRCDIR)/.config: $(CONFIG) source/$(SRCDIR)/Makefile
 	$(MAKE) -C source/$(SRCDIR) mingw64_defconfig
